216c7cfc55d8c0e049ae7912dce400042b313608
Edberaga <edbetjonnathan@gmail.com>
2024-05-15 17:47:49 +0700

    Initilize Vite app

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Footer from './component/Footer'
+import Notify from './component/Notify'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+  <main>
+    <div className="container">
+      <h2>We are Coming Soon...</h2>
+      <p>We are currently working on our new website.<br/>We will be here soon, We will notify you when we finish the Sites!</p>
+    </div>
+    <Notify/>
+    <Footer/>
+  </main>
+  )
+}
+
+export default App
