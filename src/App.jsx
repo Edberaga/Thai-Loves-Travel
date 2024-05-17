9280d3769bc8b20bd869f3e9b68f77a2d40c5d36
Edberaga <edbetjonnathan@gmail.com>
2024-05-17 23:44:58 +0700

    Add the background HD

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,20 @@ import Footer from './component/Footer'
 import Notify from './component/Notify'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
+  <>
+  <div className="background"></div>
   <main>
     <div className="container">
-      <h2>We are Coming Soon...</h2>
+      <h1>We are Coming Soon...</h1>
       <p>We are currently working on our new website.<br/>We will be here soon, We will notify you when we finish the Sites!</p>
       <Notify/>
     </div>
-    
     <Footer/>
   </main>
+  </>
+    
   )
 }
 
